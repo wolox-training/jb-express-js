ebaf9f7cbff16e1efcb96c561835d73fbf6c5f1d
javerBenavidez <javer.a.benavidez@wolox.co>
2021-11-04 15:18:41 -0500

    add tests

---
+++ test/signup/signup.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Tests sign up', () => {
+  const user = {
+    name: 'pepito',
+    last_name: 'Perez',
+    mail: 'pepito@wolox.co',
+    pass: 'PassPepit0'
+  };
+
+  test('Successful transaction', async done => {
+    await request(app)
+      .post('/users')
+      .send(user)
+      .expect(201);
+    return done();
+  });
+  test('Email already exists', async done => {
+    await request(app)
+      .post('/users')
+      .send(user);
+    await request(app)
+      .post('/users')
+      .send(user)
+      .expect(400);
+    return done();
+  });
+  test('invalid pass', async done => {
+    const userPassInvalid = user;
+    userPassInvalid.pass = 'pepito';
+    await request(app)
+      .post('/users')
+      .send(userPassInvalid)
+      .expect(400);
+    return done();
+  });
+  test('Left required params', async done => {
+    await request(app)
+      .post('/users')
+      .send({})
+      .expect(400);
+    return done();
+  });
+});
